49c2d154b69f62906522c78a7f6372e2b1b886bf
jardelcruz1 <jardelcruz@gmail.com>
2021-09-15 10:23:34 -0300

    versao 2

--- CMV ESTOQUE.py
+++ CMV ESTOQUE.py
@@ -1,9 +1,19 @@
 #codigo criado para calculo de cmv ( custo mercadoria vendida) em Reais R$
+#versao com repeticao e contador de estoque = mais de uma compra
+
 
 EstoqueInicial = float(input(' Insira o estoque inicial da fabrica:')) #valores em float pois se trata de dinheiro.
 Compras = float(input('Insira os valores de compras:'))
 Vendas = float(input('Insira o valor de venda:'))
 
-CMV = EstoqueInicial + Compras - Vendas
+CMV = (EstoqueInicial + Compras - Vendas)
 
-print(CMV)
+if (CMV > 0):
+    print(CMV)
+
+else:
+    print('Verificar possiveis omissões de notas fiscais')
+
+
+
+
